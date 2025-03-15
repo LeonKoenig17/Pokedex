1c0f1f336a05bba0ce0a5e6579776859699dd191
Beslim17 <b.montiari@gmail.com>
2025-03-16 00:48:33 +0100

    boxContent finished

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -3,6 +3,8 @@ function openOverlay(id) {
     const cardDisplay = document.getElementById("cardDisplay");
     const originalCard = document.getElementById(id);
     const card = originalCard.cloneNode(true);
+    document.getElementById("stats").classList.add("active");
+    document.getElementById("statsBtn").classList.add("activeBtn");
     let pokemon = pokemonArray[id];
     document.body.style.overflowY = "hidden";
     overlay.addEventListener("click", closeOverlay);
@@ -13,16 +15,44 @@ function openOverlay(id) {
     overlay.style.display = "flex";
     setPokemonHeight(pokemon);
     setStats(pokemon);
+    displayAbilities(pokemon);
+    displayEvoChain(pokemon);
 }
 
 function closeOverlay(event) {
     const overlay = document.getElementById("cardOverlay");
     const cardDisplay = document.getElementById("cardDisplay");
+    const abilities = document.getElementById("abilities");
     
     if (event.target === overlay) {
+        const evoChain = document.getElementById("evoChain");
+        evoChain.innerHTML = "";
+        const btns = document.querySelectorAll("#boxHeader button");
+        btns.forEach(btn => btn.classList.remove("activeBtn"));
+        const tabs = document.querySelectorAll("#boxContent > div");
+        tabs.forEach(tab => tab.classList.remove("active"));
+        abilities.innerHTML = "";
         cardDisplay.innerHTML = "";
         overlay.style.display = "none";
         document.body.style.overflowY = "auto";
+    }
+}
+
+function toggleTab(id) {
+    const tabs = document.querySelectorAll("#boxContent > div");
+    const btns = document.querySelectorAll("#boxHeader button");
+    btns.forEach(btn => btn.classList.remove("activeBtn"));
+    document.getElementById(id).classList.add("activeBtn");
+    tabs.forEach(tab => tab.classList.remove("active"));
+    switch (id) {
+        case "statsBtn": tabs[0].classList.add("active");
+            break;
+        case "abilitiesBtn": tabs[1].classList.add("active");
+            break;
+        case "evoChainBtn": tabs[2].classList.add("active");
+            break;
+        default:
+            break;
     }
 }
 
@@ -59,4 +89,30 @@ function setStats(pokemon) {
         let percentage = (stats[i] / maxStats[i]) * 100;
         statBars[i].style.width = `${percentage}%`;
     }
+}
+
+function displayAbilities(pokemon) {
+    const abilities = document.getElementById("abilities");
+    pokemon.abilities.forEach(ability => {
+        let name = ability.name.charAt(0).toUpperCase() + ability.name.slice(1);
+        abilities.innerHTML += `
+            <div class="ability">
+                <h3>${name}</h3>
+                <p>${ability.description}</p>
+            </div>
+        `;
+    })
+}
+
+function displayEvoChain(pokemon) {
+    const evoChain = document.getElementById("evoChain");
+    pokemon.evolutions.forEach(evo => {
+        let name = evo.name.charAt(0).toUpperCase() + evo.name.slice(1);
+        evoChain.innerHTML += `
+            <div class="evo">
+                <img src="${evo.image}" alt="evo">
+                <span>${name}</span>
+            </div>
+        `;
+    })
 }
